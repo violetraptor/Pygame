406243f741e80df42a68b55c76392b9ec65e519f
Lucy <appleseed@arcor.de>
2025-11-23 14:17:57 +0100

    Add files via upload

---
+++ pymidi.py
@@ -0,0 +1,20 @@
+# Alexander Freitag
+# Version: 2025-11-23
+# Programm zum Testen von MIDI-Tönen
+
+
+#Import
+import pygame
+
+
+# Hauptfunktion
+def main():
+    print("Programm gestartet.")
+    pygame.midi.init() # initialize the midi module
+
+
+
+    pygame.midi.quit() # uninitialize the midi module
+
+if __name__ == "__main__":
+    main()
